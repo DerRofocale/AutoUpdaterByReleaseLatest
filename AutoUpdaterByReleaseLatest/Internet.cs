5381c5894a5296fb17e817f1d91a5316a14e00c7
Дмитрий Яльчик <dmitry_yalchik@mail.ru>
2023-02-21 00:20:03 +0500

    Добавьте файлы проекта.

---
+++ AutoUpdaterByReleaseLatest/Internet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoUpdaterByReleaseLatest
+{
+    public class Internet
+    {
+        public bool OK()
+        {
+			try
+			{
+                Dns.GetHostEntry("");
+                return true;
+			}
+			catch
+			{
+                return false;
+			}
+        }
+    }
+}
